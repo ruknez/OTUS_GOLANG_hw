0be1173e217a2736f08149819def3621c96a0ed5
D <r@example.com>
2021-06-30 20:11:06 +0300

    fix golangci-lint

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -10,19 +10,16 @@ import (
 var ErrInvalidString = errors.New("invalid string")
 
 func Unpack(inPutStr string) (string, error) {
-
 	var outPutbug strings.Builder
 	var prevRun *rune
 	ignore := false
 
 	for _, run := range inPutStr {
-
 		if run == '\\' && !ignore {
 			ignore = true
 			continue
 		}
 		if unicode.IsDigit(run) && !ignore {
-
 			if prevRun == nil {
 				return "", ErrInvalidString
 			}
@@ -44,7 +41,6 @@ func Unpack(inPutStr string) (string, error) {
 		}
 		*prevRun = run
 		ignore = false
-
 	}
 	if prevRun != nil {
 		outPutbug.WriteString(string(*prevRun))
