734f0c185565b63902de2e9d6b09daecfa68f73d
D <r@example.com>
2021-07-27 23:24:40 +0300

    add tests

--- hw04_lru_cache/cache_test.go
+++ hw04_lru_cache/cache_test.go
@@ -49,8 +49,40 @@ func TestCache(t *testing.T) {
 		require.Nil(t, val)
 	})
 
+	t.Run("chek Len", func(t *testing.T) {
+		c := NewCache(3)
+
+		require.Equal(t, 0, c.Len())
+
+		for i := 0; i < 6; i++ {
+			c.Set(Key(strconv.Itoa(i)), i)
+		}
+
+		require.Equal(t, 3, c.Len())
+
+		c.Clear()
+
+		require.Equal(t, 0, c.Len())
+	})
+
 	t.Run("purge logic", func(t *testing.T) {
-		// Write me
+		c := NewCache(3)
+
+		for i := 0; i < 6; i++ {
+			c.Set(Key(strconv.Itoa(i)), i)
+		}
+
+		for i := 0; i < 3; i++ {
+			val, ok := c.Get(Key(strconv.Itoa(i)))
+			require.False(t, ok)
+			require.Equal(t, nil, val)
+		}
+
+		for i := 3; i < 6; i++ {
+			val, ok := c.Get(Key(strconv.Itoa(i)))
+			require.True(t, ok)
+			require.Equal(t, i, val)
+		}
 	})
 }
 
@@ -74,57 +106,4 @@ func TestCacheMultithreading(t *testing.T) {
 	}()
 
 	wg.Wait()
-}
-
-func TestWorkWithCach(t *testing.T) {
-	t.Run("Work with cach", func(t *testing.T) {
-		c := NewCache(3)
-
-		require.Equal(t, 0, c.Len())
-
-		for i := 0; i < 6; i++ {
-			c.Set(Key(strconv.Itoa(i)), i)
-		}
-
-		require.Equal(t, 3, c.Len())
-
-		for i := 0; i < 3; i++ {
-			val, ok := c.Get(Key(strconv.Itoa(i)))
-			require.False(t, ok)
-			require.Equal(t, nil, val)
-		}
-
-		for i := 3; i < 6; i++ {
-			val, ok := c.Get(Key(strconv.Itoa(i)))
-			require.True(t, ok)
-			require.Equal(t, i, val)
-		}
-
-		c.Clear()
-
-		require.Equal(t, 0, c.Len())
-
-		for i := 3; i < 6; i++ {
-			val, ok := c.Get(Key(strconv.Itoa(i)))
-			require.False(t, ok)
-			require.Equal(t, nil, val)
-		}
-
-		c.Set(Key(strconv.Itoa(6)), 6)
-		val, ok := c.Get(Key(strconv.Itoa(6)))
-		require.True(t, ok)
-		require.Equal(t, 6, val)
-
-		err := c.Remove(Key(strconv.Itoa(6)))
-		require.True(t, err)
-
-		val, ok = c.Get(Key(strconv.Itoa(6)))
-		require.False(t, ok)
-
-		err = c.Remove(Key(strconv.Itoa(6)))
-		require.False(t, err)
-
-		require.Equal(t, nil, val)
-		require.Equal(t, 0, c.Len())
-	})
 }
