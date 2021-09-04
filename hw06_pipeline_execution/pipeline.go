ce422850d0d3e11f0e0424b2d62ba04b80eccb3f
D <r@example.com>
2021-09-05 00:45:37 +0300

    input commit

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -9,6 +9,33 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+
+	out := make(In)
+	bichannel := make(Bi)
+	go func() {
+		for {
+			select {
+			case <-done:
+				close(bichannel)
+				return
+			case val, ok := <-in:
+				if ok {
+					bichannel <- val
+				} else {
+					close(bichannel)
+					return
+				}
+			}
+		}
+	}()
+
+	for i, stage := range stages {
+		if i == 0 {
+			out = stage(bichannel)
+		} else {
+			out = stage(out)
+		}
+	}
+
+	return out
 }
