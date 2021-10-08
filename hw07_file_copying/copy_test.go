8cec326b60d632cc6830a44d765f5b14afcd4783
Larin Dmitriy Aleksandrovich <dmlarin@ozon.ru>
2021-10-08 23:44:05 +0300

    add tests

--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,69 @@
 package main
 
-import "testing"
+import (
+	"io"
+	"io/ioutil"
+	"os"
+	"testing"
 
+	"github.com/stretchr/testify/require"
+)
+
+// Какой смысл писать тесты если уже все протестировано через bash скрипт.
 func TestCopy(t *testing.T) {
-	// Place your code here.
+	testInputString := "test date"
+	tFile, errTmp := ioutil.TempFile("", "soursFile")
+	require.NoError(t, errTmp)
+	defer tFile.Close()
+
+	outFile, errOut := ioutil.TempFile("", "outFile")
+	require.NoError(t, errOut)
+	defer outFile.Close()
+
+	tFile.WriteString(testInputString)
+
+	t.Run("sucsec test", func(t *testing.T) {
+		err := Copy(tFile.Name(), outFile.Name(), 0, 0)
+		require.NoError(t, err)
+		tFile.Seek(0, 0)
+
+		inputBuf, err := io.ReadAll(tFile)
+		require.NoError(t, err)
+		outBuf, err := io.ReadAll(outFile)
+		require.NoError(t, err)
+		require.Equal(t, inputBuf, outBuf)
+	})
+}
+
+func TestCheckInputParams(t *testing.T) {
+	testInputString := "test date"
+	tFile, errTmp := ioutil.TempFile("", "soursFile")
+	require.NoError(t, errTmp)
+	defer tFile.Close()
+
+	tFile.WriteString(testInputString)
+
+	t.Run("no input file", func(t *testing.T) {
+		inputSize, err := checkInputParams("LOLOLO", "out", 1)
+		require.Equal(t, inputSize, int64(0))
+		require.True(t, os.IsNotExist(err))
+	})
+
+	t.Run("there is input file", func(t *testing.T) {
+		inputSize, err := checkInputParams(tFile.Name(), "out", 0)
+		require.Equal(t, inputSize, int64(len(testInputString)))
+		require.NoError(t, err)
+	})
+
+	t.Run("offset exceeds file size", func(t *testing.T) {
+		inputSize, err := checkInputParams(tFile.Name(), "out", int64(len(testInputString))+10)
+		require.Equal(t, inputSize, int64(0))
+		require.Equal(t, err, ErrOffsetExceedsFileSize)
+	})
+
+	t.Run("the same files", func(t *testing.T) {
+		inputSize, err := checkInputParams(tFile.Name(), tFile.Name(), 0)
+		require.Equal(t, inputSize, int64(0))
+		require.Equal(t, err, ErrTheSameFiles)
+	})
 }
--- hw07_file_copying/go.mod
+++ hw07_file_copying/go.mod
@@ -2,4 +2,7 @@ module github.com/ruknez/OTUS_GOLANG_hw/hw07_file_copying
 
 go 1.16
 
-require github.com/cheggaaa/pb/v3 v3.0.8
+require (
+	github.com/cheggaaa/pb/v3 v3.0.8
+	github.com/stretchr/testify v1.7.0
+)
