6e817200f92cd39ac07d0ced6a6f16f2c6fd95cf
ruknez <ruknez94@mail.ru>
2022-01-08 16:05:23 +0300

    start hw 08 test

--- hw08_envdir_tool/env_reader.go
+++ hw08_envdir_tool/env_reader.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
 type Environment map[string]EnvValue
 
 // EnvValue helps to distinguish between empty files and files with the first empty line.
@@ -11,6 +17,29 @@ type EnvValue struct {
 // ReadDir reads a specified directory and returns map of env variables.
 // Variables represented as files where filename is name of variable, file first line is a value.
 func ReadDir(dir string) (Environment, error) {
-	// Place your code here
+	files, err := os.ReadDir(".")
+	if err != nil {
+		return nil, err
+	}
+	env :=
+
+	resultEnv := make(Environment, len(files))
+	for _, file := range files {
+		if file.IsDir() {
+			return nil, fmt.Errorf("it is  dir = %s", file.Name())
+		}
+
+	}
+
 	return nil, nil
+}
+
+func main() {
+	allCommandLineArgs := os.Args[1:]
+	if len(allCommandLineArgs) < 2 {
+		log.Fatalf("Not enough args = %s", allCommandLineArgs)
+	}
+
+	ReadDir(allCommandLineArgs[0])
+
 }
